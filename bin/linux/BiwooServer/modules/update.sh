4de1f1ed8983db0886f77c648cffe9520a0c1cc6
akee.yang@gmail.com <akee.yang@gmail.com@2d2a4f7e-5a34-d277-b151-81068e2f7590>
2010-06-07 06:38:26 +0000

    linux compile.

--- bin/linux/BiwooServer/modules/update.sh
+++ bin/linux/BiwooServer/modules/update.sh
@@ -9,5 +9,6 @@ cp -f /usr/local/lib/libCommUdpServer.so libP2PUDPServer.so
 cp -f /usr/local/lib/libParserSotp.so .
 cp -f /usr/local/lib/libRtpService.so .
 cp -f /usr/local/lib/libSipService.so .
+cp -f /usr/local/lib/libBodbService.so .
 cp -f /usr/local/lib/libStreamModule.so .
 
